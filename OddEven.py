c1fb5cd42b9041c8e7daf6d9d6e6d39b4a45d86a
ysaurabh <saurabhyadav900400@gmail.com>
2016-10-08 09:25:42 +0530

    Added functions

--- OddEven.py
+++ OddEven.py
@@ -1,6 +1,19 @@
-# program to identify the OddEven state
-number=int(input("Enter a number"))
-if number % 2==0:
-               print("the entered number is even")
-else:
-               print("the entered number is odd") 
+# Python progranm to find the factorial of a number provided
+#function to print factorial of given number
+
+def isOddEven (number):
+    factorial = 1
+    if number < 0:
+        print("Sorry, factorial does not exist for negative number")
+    elif number < 0:
+        print("The factorial of 0 is 1")
+    else:
+        for i in range(1,number + 1):
+                factorial = factorial*i
+        print("The factorial of",number,"is",factorial)
+
+# Change the value for a different result
+num = int(input("enter  a number:"))
+
+isOddEven (num)
+        
